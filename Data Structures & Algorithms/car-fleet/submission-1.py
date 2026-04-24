5b2bf15c3c4c7c196a60383073a6ae64317a4627
chanbin park <chanbinp3@gmail.com>
2026-04-24 18:11:30 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # stack of times that you look at from closest to target
+        # at any point if the time to reach target of a rear fleet
+        # is less than time of fleet infront you pop that cause it
+        # becomes part of the front fleet
+
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+
+        stack = []
+
+        for p, s in pair:
+            time = target - p
+            time = time / s
+            stack.append(time)
+            
+            # if the current car reaches target at same time or earlier than car in front (fleet so pop)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
